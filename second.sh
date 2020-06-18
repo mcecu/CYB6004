5d3a99270a286dc5893982a921cf87e9231ab77d
mcecu <66341554+mcecu@users.noreply.github.com>
2020-06-18 12:54:18 +1000

    Add files via upload

---
+++ second.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#the script required no significant changes from the example provided
+#an alternative may be to declare the variable name=Matt in the script
+
+echo "Hi there!"
+echo "It's good to see you $1!"
+exit 0
