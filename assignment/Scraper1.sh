98835da9ab088aa04dc1c397303563bb96742955
mcecu <66341554+mcecu@users.noreply.github.com>
2020-06-20 14:41:18 +1000

    Add files via upload
    Have added a case statement to allow users to choose which option they would like to display. Am now looking to use awk to format data and improve readability from an end user standpoint

--- assignment/Scraper1.sh
+++ assignment/Scraper1.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
-until [ "$RESPONSE" != "exit" ]
-do
-  echo -n "Please enter the URL of a file to download, or type exit to quit: "
-  read RESPONSE
-  if [ "$RESPONSE" != "exit" ]; then
+curl -o output.txt https://www.cisco.com/c/en_au/products/security/what-is-cybersecurity.html#~how-cybersecurity-works
 
-cp "$RESPONSE" output.txt 
+cat output.txt | grep "<p>" | sed 's/<[^>]*>//g' > temp.txt
 
-fi
-done
+echo "$(tput setaf 3)SELECT AN OPTION
+$(tput setaf 4)1. Phishing 
+2. Ransomware
+3. Malware
+4. Social Engineering
+5. Exit"
+
+read CHOICE
+
+case $CHOICE in
+
+1) 
+cat temp.txt | sed -n -e 12p 
+;;
+
+2)
+cat temp.txt | sed -n -e 14p 
+;;
+
+3) 
+cat temp.txt | sed -n -e 16p | sed 's/&nbsp;//g'
+;;
+
+4)
+cat temp.txt | sed -n -e 18p 
+;;
+
+5)
+exit 0
+;;
+
+esac
 
