49ea2a47b975b900396bbe5f3d986b9294e43a69
mcecu <66341554+mcecu@users.noreply.github.com>
2020-06-20 18:39:33 +1000

    Add files via upload
    Added some additional features such as a while loop to ensure the script repeats after an option has been chosen. I have made improvements to the formatting too.

--- assignment/Scraper1.sh
+++ assignment/Scraper1.sh
@@ -4,36 +4,41 @@ curl -o output.txt https://www.cisco.com/c/en_au/products/security/what-is-cyber
 
 cat output.txt | grep "<p>" | sed 's/<[^>]*>//g' > temp.txt
 
-echo "$(tput setaf 3)SELECT AN OPTION
-$(tput setaf 4)1. Phishing 
-2. Ransomware
-3. Malware
-4. Social Engineering
-5. Exit"
+while true; do
+   choice=
+   while [[ -z $choice ]]; do
+    echo "$(tput setaf 6)SELECT AN OPTION
+    $(tput setaf 2)1. Phishing 
+    2. Ransomware
+    3. Malware
+    4. Social Engineering
+    $(tput setaf 1)5. Exit"
 
 read CHOICE
 
 case $CHOICE in
 
 1) 
-cat temp.txt | sed -n -e 12p 
+cat temp.txt | sed -n -e 12p | fmt
 ;;
 
 2)
-cat temp.txt | sed -n -e 14p 
+cat temp.txt | sed -n -e 14p | fmt
 ;;
 
 3) 
-cat temp.txt | sed -n -e 16p | sed 's/&nbsp;//g'
+cat temp.txt | sed -n -e 16p | sed 's/&nbsp;//g' | fmt
 ;;
 
 4)
-cat temp.txt | sed -n -e 18p 
+cat temp.txt | sed -n -e 18p | fmt
 ;;
 
 5)
-exit 0
+ echo "Quitting ..."
+ exit 0
 ;;
 
 esac
-
+done
+done
