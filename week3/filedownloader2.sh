c81a959b804f157c5a79e73099cc5b40b684d278
mcecu <66341554+mcecu@users.noreply.github.com>
2020-06-22 13:38:27 +1000

    Delete filedownloader2.sh
    duplicate

--- week3/filedownloader2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Please type the URL of a file to download or type 'exit' to quit: " URL
-read -p "Please type the location of where you would like to send the download to: " path
-
-until [ $URL = "exit" ]; 
-do
-	wget -O $path $URL
-done
